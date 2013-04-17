b03a487ff6334461c872e838ae099c5fcdf2fdd8
Peter Keating <peter@moov2.com>
2013-04-17 23:05:17 +0100

    moved the construction of arguments from the options to outside the file loop so the same code with the same outcome isn't run multiple times.

--- tasks/csscss.js
+++ tasks/csscss.js
@@ -20,103 +20,105 @@ module.exports = function(grunt) {
 
     var done = this.async();
 
+    var args = [];
+
+    /**
+     * Outputs the rules that have been matched.
+     */
+    if (options.verbose) {
+      args.push('-v');
+    }
+
+    /**
+     * Flag indicating whether the output should be colorized. This is true by
+     * default.
+     */
+    if (options.colorize === false) {
+      args.push('--no-color');
+    }
+
+    /**
+     * Enables Compass extensions when parsing Sass files. This argument is not
+     * set if the compassConfig property has been defined.
+     */
+    if (options.compass && !options.compassConfig) {
+      args.push('--compass');
+    }
+
+    /**
+     * Enables Compass extensions when parsing Sass files and specifies the path
+     * to the config file.
+     */
+    if (options.compassConfig) {
+      args.push('--compass-with-config');
+      args.push(options.compassConfig);
+    }
+
+    /**
+     * Returns analysis in JSON format.
+     */
+    if (options.outputJson) {
+      args.push('-j');
+    }
+
+    /**
+     * Sets the minimum number of rules before a match is found.
+     */
+    if (options.minMatch) {
+      args.push('-n');
+      args.push(options.minMatch);
+    }
+
+    /**
+     * Sets which properties should be ignored.
+     */
+    if (options.ignoreProperties) {
+      args.push('--ignore-properties');
+      args.push(options.ignoreProperties);
+    }
+
+    /**
+     * Sets which selectors should be ignored.
+     */
+    if (options.ignoreSelectors) {
+      args.push('--ignore-selectors');
+      args.push(options.ignoreSelectors);
+    }
+
+    /**
+     * Sets whether parser errors should be outputted.
+     */
+    if (options.showParserErrors) {
+      args.push('--show-parser-errors');
+    }
+
+    /**
+     * Sets whether shorthand should be matched.
+     */
+    if (options.shorthand === false) {
+      args.push('--no-match-shorthand');
+    }
+
     grunt.util.async.forEachSeries(this.data.src, function(f, next) {
-      var args = [];
 
       /**
-       * Outputs the rules that have been matched.
+       * adds the file path as the final argument, this goes into a new array so
+       * the file doesn't get used in the next iteration.
        */
-      if (options.verbose) {
-        args.push('-v');
-      }
-
-      /**
-       * Flag indicating whether the output should be colorized. This is true by
-       * default.
-       */
-      if (options.colorize === false) {
-        args.push('--no-color');
-      }
-
-      /**
-       * Enables Compass extensions when parsing Sass files. This argument is not
-       * set if the compassConfig property has been defined.
-       */
-      if (options.compass && !options.compassConfig) {
-        args.push('--compass');
-      }
-
-      /**
-       * Enables Compass extensions when parsing Sass files and specifies the path
-       * to the config file.
-       */
-      if (options.compassConfig) {
-        args.push('--compass-with-config');
-        args.push(options.compassConfig);
-      }
-
-      /**
-       * Returns analysis in JSON format.
-       */
-      if (options.outputJson) {
-        args.push('-j');
-      }
-
-      /**
-       * Sets the minimum number of rules before a match is found.
-       */
-      if (options.minMatch) {
-        args.push('-n');
-        args.push(options.minMatch);
-      }
-
-      /**
-       * Sets which properties should be ignored.
-       */
-      if (options.ignoreProperties) {
-        args.push('--ignore-properties');
-        args.push(options.ignoreProperties);
-      }
-
-      /**
-       * Sets which selectors should be ignored.
-       */
-      if (options.ignoreSelectors) {
-        args.push('--ignore-selectors');
-        args.push(options.ignoreSelectors);
-      }
-
-      /**
-       * Sets whether parser errors should be outputted.
-       */
-      if (options.showParserErrors) {
-        args.push('--show-parser-errors');
-      }
-
-      /**
-       * Sets whether shorthand should be matched.
-       */
-      if (options.shorthand === false) {
-        args.push('--no-match-shorthand');
-      }
-
-      /**
-       * adds path to file, to be analysed, as an argument.
-       */
-      args.push(f);
+      var cmdArgs = args.concat([f]);
 
       /**
        * Outputs the file that is being analysed.
        */
       grunt.log.writeln(f);
-      grunt.verbose.writeln('csscss ' + args.join(' '));
+      grunt.verbose.writeln('csscss ' + cmdArgs.join(' '));
 
       /**
        * Executes the csscss command.
        */
       var child = grunt.util.spawn({
         cmd: 'csscss',
-        args: args
+        args: cmdArgs
       }, function(error, result, code) {
         if (code === 127) {
           return grunt.warn('Ruby and csscss have to be installed and in your PATH for this task to run.');
